8bebe069b1f8e8c5b851a2c2823996a00095df83
CambiG1123 <campbellgilliland@gmail.com>
2023-08-13 23:54:48 -0400

    fixed bug where nav toggler wouldn't reappear at top

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,9 +1,21 @@
+var width = $(innerWidth)
+var scrollPos = document.documentElement.scrollTop || document.body.scrollTop
+// var showToggle = $(window)
+
 $(window).scroll(function() {
     if ($(window).scrollTop() > 50) {
         $('#navToggle').hide();
     }
     else {
+        
+        // if ($(innerWidth) < 980 && $(window).scrollTop() > 50)
         $('#navToggle').show();
     }
 });
 
+$(window).width(function(){
+    if (window.width < 900 && scrollPos>50){
+        $('#navToggle').show();
+
+    }
+})
