0ed7da85b2241f372846ed923307358e0d5f43a2
Rachel Hochman <136940837+RachelCodes42@users.noreply.github.com>
2023-08-14 19:49:19 -0400

    quotes for carousel js array

--- assets/js/quoteobjarray.js
+++ assets/js/quoteobjarray.js
@@ -1,77 +1,134 @@
 var quotesObjArray = [
     {
-        name:'-Carl Sagan: Astronomer',
-            quote: '“The universe is a pretty big place. If it’s just us, seems like an awful waste of space.”'
-
+        name: '-Carl Sagan: Astronomer',
+        quote: '“The universe is a pretty big place. If it’s just us, seems like an awful waste of space.”'
     },
     {
-        name:'-Carl Sagan: Astronomer',
-            quote: '“It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring.”'
+        name: '-Carl Sagan: Astronomer',
+        quote: '“It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring.”'
     },
     {
-
         name: '-Neil Armstrong: First astronaut to walk on the Moon',
-            quote: '”I didn’t feel like a giant. I felt very, very small.”'
+        quote: '”I didn’t feel like a giant. I felt very, very small.”'
     },
     {
         name: '-Stephen Hawking: Theoretical physicist',
-            quote: '“I don’t think the human race will survive the next thousand years unless we spread into space.'
+        quote: '“I don’t think the human race will survive the next thousand years unless we spread into space.”'
     },
     {
         name: '-Christa McAuliffe: Teacher and astronaut',
-            quote: '“Space is for everybody. It’s not just for a few people in science or math, or for a select group of astronauts. That’s our new frontier out there, and it’s everybody’s business to know about space.”'
+        quote: '“Space is for everybody. It’s not just for a few people in science or math, or for a select group of astronauts. That’s our new frontier out there, and it’s everybody’s business to know about space.”'
     },
     {
-
         name: '-Konstantin Tsiolkovsky: Rocket scientist who pioneered astronautics',
-            quote: '“Earth is the cradle of humanity, but one cannot live in a cradle forever.”'
+        quote: '“Earth is the cradle of humanity, but one cannot live in a cradle forever.”'
     },
     {
         name: "Douglas Adams: Author of `The Hitchhiker's Guide to the Galaxy`",
-            quote: '“Looking up into the night sky is looking into infinity-distance is incomprehensible and therefore meaningless.”'
+        quote: '“Looking up into the night sky is looking into infinity—distance is incomprehensible and therefore meaningless.”'
     },
     {
-        name: '- Socrates: Philosopher',
-            quote: '"Man must rise above the Earth—to the top of the atmosphere and beyond—for only thus will he fully understand the world in which he lives."'
+        name: '-Socrates: Philosopher',
+        quote: '"Man must rise above the Earth—to the top of the atmosphere and beyond—for only thus will he fully understand the world in which he lives."'
     },
     {
-
-        name: '- Miles Kington: Journalist',
-            quote: '"Astronomers, like burglars and jazz musicians, operate best at night."'
+        name: '-Miles Kington: Journalist',
+        quote: '"Astronomers, like burglars and jazz musicians, operate best at night."'
     },
     {
-        name: '- Edwin Hubble: Astronomer',
-            quote: '"Equipped with his five senses, man explores the universe around him and calls the adventure Science."'
+        name: '-Edwin Hubble: Astronomer',
+        quote: '"Equipped with his five senses, man explores the universe around him and calls the adventure Science."'
     },
     {
-        name: '- Sir Fred Hoyle: Astronomer',
-            quote: `"Space isn't remote at all. It's only an hour's drive away, if your car could go straight upwards."`
+        name: '-Sir Fred Hoyle: Astronomer',
+        quote: `"Space isn't remote at all. It's only an hour's drive away, if your car could go straight upwards."`
     },
     {
-
         name: '-Carl Sagan: Astronomer',
-            quote: `"Somewhere, something incredible is waiting to be known."`
+        quote: `"Somewhere, something incredible is waiting to be known."`
     },
     {
-
         name: '-Arthur C. Clarke: Science fiction author and futurist',
-            quote: '"Astronomy, as nothing else can do, teaches men humility"'
+        quote: '"Astronomy, as nothing else can do, teaches men humility."'
     },
     {
-
         name: '-Kurt Vonnegut: Author and humorist',
-            quote: '"The universe is a big place, perhaps the biggest."'
+        quote: '"The universe is a big place, perhaps the biggest."'
     },
     {
-
         name: '-Peter De Vries: Editor and novelist',
-            quote: '"The universe is like a safe to which there is a combination. But the combination is locked up in the safe."'
+        quote: '"The universe is like a safe to which there is a combination. But the combination is locked up in the safe."'
     },
     {
-
         name: '-John Scott Russell: Civil engineer',
-            quote: '"Astronomy is the science of the harmony of infinite expanse."'
+        quote: '"Astronomy is the science of the harmony of infinite expanse."'
     },
+    {
+        name: '-Jocelyn Bell Burnell: Astrophysicist',
+        quote: '"Astronomy is a world of extremes, where even the cold is hot and the largest things can turn out to be the smallest."'
+    },
+    {
+        name: '-Vera Rubin: Astronomer',
+        quote: '"Don’t be afraid of the space between your dreams and reality. If you can dream it, you can make it so."'
+    },
+    {
+        name: '-Vera Rubin: Astronomer',
+        quote: '"Science progresses best when observations force us to alter our preconceptions."'
+    },
+    {
+        name: '-Albert Einstein: Theoretical physicist',
+        quote: '"The most incomprehensible thing about the universe is that it is comprehensible."'
+    },
+    {
+        name: '-Alan Stern: Planetary scientist',
+        quote: '"We should always keep in mind that someday our descendants will venture to other stars, not by the hundreds or thousands, but in huge self-sustaining ships, with entire cities onboard."'
+    },
+    {
+        name: '-Annie Jump Cannon: Astronomer',
+        quote: '"Classification is the beginning of wisdom."'
+    },
+    {
+        name: '-Edwin Powell Hubble: Astronomer',
+        quote: '"The history of astronomy is a history of receding horizons."'
+    },
+    {
+        name: '-Sylvia Earle: Marine biologist and explorer',
+        quote: '"Even if you never have the chance to see or touch the ocean, the ocean touches you with every breath you take, every drop of water you drink, every bite you consume. Everyone, everywhere is inextricably connected to and utterly dependent upon the existence of the sea."'
+    },
+    {
+        name: '-Maria Mitchell: Astronomer',
+        quote: '"We especially need imagination in science. It is not all mathematics, nor all logic, but it is somewhat beauty and poetry."'
+    },
+    {
+        name: '-Helen Sharman: First British astronaut',
+        quote: '"The planet is too small for nationalism. Our job is to protect our fragile world for all its people."'
+    },
+    {
+        name: '-Alan Bean: Apollo 12 astronaut and artist',
+        quote: '"I’m not afraid to die. I’m just afraid I haven’t lived enough yet."'
+    },
+    {
+        name: '-Cecilia Payne-Gaposchkin: Astronomer',
+        quote: '"The reward of the young scientist is the emotional throb of being the first person in the history of the world to see something or to understand something. Nothing can compare with that experience."'
+    },
+    {
+        name: '-Neil deGrasse Tyson: Astrophysicist',
+        quote: '"The universe is under no obligation to make sense to you."'
+    },
+    {
+        name: '-Eileen Collins: First female Space Shuttle Commander',
+        quote: '"I was always hoping there’d be something that would excite me as much as flying in space."'
+    },
+    {
+        name: '-Aristotle: Philosopher',
+        quote: '"In all things of nature there is something of the marvelous."'
+    },
+    {
+        name: '-Johannes Kepler: Astronomer',
+        quote: '"The diversity of the phenomena of nature is so great, and the treasures hidden in the heavens so rich, precisely in order that the human mind shall never be lacking in fresh nourishment."'
+    }
+];
+    
 
 
 
@@ -85,13 +142,3 @@ var quotesObjArray = [
 
 
 
-
-
-
-
-
-
-
-
-
-]
